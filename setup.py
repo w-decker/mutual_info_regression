2e85aba0390ab4e70c0a868c2df7f373b9899c7a
Will Decker <will.decker@gatech.edu>
2025-04-21 13:12:57 -0400

    initial commit --> make installable

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mutual_info_regression",
+    version="0.0.1",
+    author="Will Decker",
+    author_email="will.decker@gatech.edu",
+    description="Estimate mutual information for a continuous target variable using Holmes and Nemenman (2019) estimator",
+    url="https://github.com/w-decker/mutual_info_regression",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering"
+    ]
+
+)
